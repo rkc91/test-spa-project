05b7761ffbc8e58d71c9d71ff4e59a1681fee1ea
rkc91 <rcasey391@gmail.com>
2025-03-23 19:32:33 -0400

    feat: introduced new fonts and css. More navbar work

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,13 +4,13 @@ import '../styling/components/Navbar.css';
 
 const MyNavbar = () => {
     return (
-        <Navbar bg="dark" variant="dark" className="navbar">
+        <Navbar fixed="top" bg="dark" variant="dark" className="navbar" expand="lg">
                 <Navbar.Brand as={NavLink} to="/">
-                <Image src="./img/hi_logo.png" className="navbar-logo"/>
+                <Image src="./img/hi_logo_named.png" className="navbar-logo"/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav>
                         <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
                         <NavDropdown title="About Us" id="about-us">
                             <NavDropdown.Item as={NavLink} to="/about">About</NavDropdown.Item>
@@ -18,13 +18,13 @@ const MyNavbar = () => {
                         <NavDropdown title="Join Us" id="join-us">
                             <NavDropdown.Item as={NavLink} to='/join-us'>Join Horizon Initiative</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title="Community" id="community">
+                        <NavDropdown title="Community" id="community" >
                             <NavDropdown.Item as={NavLink} to='/events'>Guild Events</NavDropdown.Item>
                             <NavDropdown.Item as={NavLink} to='/roster'>Guild Roster</NavDropdown.Item>
                             <NavDropdown.Item as={NavLink} to='/streams'>Live Streams</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link as={NavLink} to='/store'>Guild Store</Nav.Link>
-                        <Nav.Link as={NavLink} to='/contact-us'>Contact Us</Nav.Link>
+                        <Nav.Link as={NavLink} to='/store' >Guild Store</Nav.Link>
+                        <Nav.Link as={NavLink} to='/contact-us' >Contact Us</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
         </Navbar>
